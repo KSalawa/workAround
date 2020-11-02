6e329d768004aa47efdfacecd5cb859076230a02
Kamil Salawa <emir.salawa@gmail.com>
2020-11-02 10:35:35 +0000

    final version 666

--- workAround.mjs
+++ workAround.mjs
@@ -1,14 +1,16 @@
+import { getCadre, calculateTax, getBenefits, calculateBonus, reimbursementEligibility } from './employee.mjs';
+
 import Employee from './employee.mjs';
 
-function getEmployeeInformation(inputSalary) {
-    Employee.salary = inputSalary;
-    console.log('Cadre: ' + Employee.getCadre());
-    console.log('Tax: ' + Employee.calculateTax());
-    console.log('Benefits: ' + Employee.getBenefits());
-    console.log('Bonus: ' + Employee.calculateBonus());
-    console.log('Reimbursement Eligibility: ' + Employee.reimbursementEligibility() + '\n');
-  }
-  
-  getEmployeeInformation(10000);
-  getEmployeeInformation(50000);
-  getEmployeeInformation(100000);
+function getEmployeeInformation(salary) {
+  Employee.salary = salary;
+  console.log('Cadre: ' + getCadre());
+  console.log('Tax: ' + calculateTax());
+  console.log('Benefits: ' + getBenefits());
+  console.log('Bonus: ' + calculateBonus());
+  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+}
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
