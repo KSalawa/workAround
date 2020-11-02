8b68b2ec7ca8021606228071999fd3d9569eb612
Kamil Salawa <emir.salawa@gmail.com>
2020-11-02 10:27:37 +0000

    done

--- workAround.mjs
+++ workAround.mjs
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { getCadre, calculateTax, getBenefits, calculateBonus, reimbursementEligibility } from './employee.mjs';
-
-import Employee from './employee.mjs';
-
-function getEmployeeInformation(inputSalary) {
-  Employee.salary = inputSalary;
-  console.log('Cadre: ' + getCadre());
-  console.log('Tax: ' + calculateTax());
-  console.log('Benefits: ' + getBenefits());
-  console.log('Bonus: ' + calculateBonus());
-  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
-}
-
-getEmployeeInformation(10000);
-getEmployeeInformation(50000);
-getEmployeeInformation(100000);
-=======
 import Employee from './employee.mjs';
 
 function getEmployeeInformation(inputSalary) {
@@ -30,4 +12,3 @@ function getEmployeeInformation(inputSalary) {
   getEmployeeInformation(10000);
   getEmployeeInformation(50000);
   getEmployeeInformation(100000);
->>>>>>> 2a095523eec266fca08425521de9b7fc6785fbf2
